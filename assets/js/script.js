946347de42ab4d2229243e4e14eb9432ac9d9bc1
Salman Ahmed <sscvocational4@gmail.com>
2024-12-09 20:52:32 +0600

    change is made in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+if (navToggle) {
+  navToggle.addEventListener("click", () =>
+    navMenu.classList.add("show-menu")
+  );
+}
 
+if (navClose) {
+  navClose.addEventListener("click", () =>
+    navMenu.classList.remove("show-menu")
+  );
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
 /*=============== SWIPER POPULAR ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
