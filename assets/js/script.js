b2cddc51a7460385282538532a717e10d98ff21c
Salman Ahmed <sscvocational4@gmail.com>
2024-12-09 21:53:21 +0600

    completing the navbar. fully responsive
    completing the navbar. Making it fully responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll("#nav-link")
 
 if (navToggle) {
   navToggle.addEventListener("click", () =>
@@ -13,6 +14,17 @@ if (navClose) {
   navClose.addEventListener("click", () =>
     navMenu.classList.remove("show-menu")
   );
+
+  if (navLink) {
+    navLink.forEach(link => {
+        link.addEventListener("click",()=>{
+
+            navMenu.classList.remove("show-menu")
+
+        })
+    });
+    
+}
 }
 /*=============== REMOVE MENU MOBILE ===============*/
 
